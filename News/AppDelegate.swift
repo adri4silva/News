f870ebe8e45a2387735c572074341b4c5cbf2d13
Adri Silva <adri4silva@gmail.com>
2019-02-05 09:40:02 +0100

    Project initial setup

---
+++ News/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  News
+//
+//  Created by Adrián Silva on 04/02/2019.
+//  Copyright © 2019 Adrián Silva. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    let window: UIWindow = UIWindow()
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        return true
+    }
+}
+
---
+++ NewsCoreTests/NewsCoreTests.swift
@@ -0,0 +1,22 @@
+//
+//  NewsCoreTests.swift
+//  NewsCoreTests
+//
+//  Created by Adrián Silva on 05/02/2019.
+//  Copyright © 2019 Adrián Silva. All rights reserved.
+//
+
+import XCTest
+
+class NewsCoreTests: XCTestCase {
+
+    override func setUp() {
+        
+        
+    }
+
+    override func tearDown() {
+        
+        
+    }
+}
